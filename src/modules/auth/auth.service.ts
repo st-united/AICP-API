6b9a924f5368cdd807e918d45e45ec75fc5034a5
bxthien <buixuanthien.0105@gmail.com>
2025-05-19 16:41:32 +0700

    feat: [AICP-37] update message when register

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -120,7 +120,11 @@ export class AuthService {
     );
     await this.emailService.sendActivationEmail(user.fullName, user.email, activationToken);
 
-    return new ResponseItem(user, 'Đăng ký thành công', UserResponseDto);
+    return new ResponseItem(
+      user,
+      'Đăng ký tài khoản thành công. Vui lòng kiểm tra email để kích hoạt tài khoản.',
+      UserResponseDto
+    );
   }
 
   async activateAccount(token: string): Promise<ResponseItem<null>> {
