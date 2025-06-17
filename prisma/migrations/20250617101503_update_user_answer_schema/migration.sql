0e9bf182b8a96cbc117c325ec0d50724d67d1b4b
Dang Van Sinh <sinh.dang@stunited.vn>
2025-06-17 17:16:33 +0700

    feat: [AICP-53] Update function to save answers as draft

---
+++ prisma/migrations/20250617101503_update_user_answer_schema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `max_possible_score` on the `UserAnswer` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserAnswer" DROP COLUMN "max_possible_score";
