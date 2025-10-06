a55c9fb21420d614d2125023edb3b7b10c233e4d
hoanglongle88 <long.le@stunited.vn>
2025-10-06 10:07:14 +0700

    refactor: [AICPS-206] Change response format of get all universities

--- src/modules/universities/dto/request/get-universities.dto.ts
+++ src/modules/universities/dto/request/get-universities.dto.ts
@@ -1,20 +1,10 @@
 import { IsString, IsOptional, IsInt, Min } from 'class-validator';
 import { Type } from 'class-transformer';
+import { extend } from 'joi';
+import { PageOptionsDto } from '@app/common/dtos';
 
-export class GetUniversitiesDto {
+export class GetUniversitiesDto extends PageOptionsDto {
   @IsString()
   @IsOptional()
   search?: string;
-
-  @IsInt()
-  @Min(0)
-  @IsOptional()
-  @Type(() => Number)
-  skip?: number = 0;
-
-  @IsInt()
-  @Min(1)
-  @IsOptional()
-  @Type(() => Number)
-  take?: number = 100;
 }
--- src/modules/universities/dto/university.dto.ts
+++ src/modules/universities/dto/university.dto.ts
@@ -1,12 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 import { IsInt, IsString, IsUUID, Min } from 'class-validator';
 
 export class University {
   @IsUUID()
+  @Expose()
+  @ApiProperty()
   id: string;
 
   @IsString()
+  @Expose()
+  @ApiProperty()
   name: string;
 
   @IsString()
+  @Expose()
+  @ApiProperty()
   code: string;
 }
