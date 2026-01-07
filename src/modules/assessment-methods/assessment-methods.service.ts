350310e4171fe808ca51d2da937183b7d8317ada
a-Minh <minh.la@s.dgwasia.com>
2026-01-07 11:07:11 +0700

    feat: [AICPS-262] create new assessment method

--- src/modules/assessment-methods/assessment-methods.service.ts
+++ src/modules/assessment-methods/assessment-methods.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, NotFoundException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
-import { ResponseAssessmentMethodDto } from './dto/response/response-assessment-method.dto';
-import { PageMetaDto, ResponsePaginate } from '@app/common/dtos';
+import { MutateAssessmentMethodDto } from './dto/request/mutate-assessment-method.dto';
+import { PageMetaDto, ResponseItem, ResponsePaginate } from '@app/common/dtos';
 import { RequestListAssessmentMethodDto } from './dto/request/request-list-assessment-method.dto';
 import { plainToInstance } from 'class-transformer';
 import { convertStringToEnglish } from '@app/common/utils';
+import { ResponseAssessmentMethodDto } from './dto/response/response-assessment-method.dto';
 
 @Injectable()
 export class AssessmentMethodsService {
@@ -40,5 +41,58 @@ export class AssessmentMethodsService {
       pageMetaDto,
       'Lấy danh sách phương thức đánh giá thành công'
     );
+  }
+
+  async create(dto: MutateAssessmentMethodDto): Promise<ResponseItem<ResponseAssessmentMethodDto>> {
+    const existingMethod = await this.prisma.assessmentMethod.findUnique({
+      where: { name: dto.name },
+    });
+
+    if (existingMethod) {
+      throw new ConflictException('Phương thức đánh giá đã tồn tại.');
+    }
+
+    const method = await this.prisma.assessmentMethod.create({
+      data: {
+        name: dto.name,
+        description: dto.description || null,
+        searchText: convertStringToEnglish(dto.name, true),
+      },
+    });
+    return new ResponseItem(
+      plainToInstance(ResponseAssessmentMethodDto, method, {
+        excludeExtraneousValues: true,
+      }),
+      'Tạo phương thức đánh giá thành công'
+    );
+  }
+  async update(id: string, dto: MutateAssessmentMethodDto): Promise<ResponseItem<ResponseAssessmentMethodDto>> {
+    try {
+      const method = await this.prisma.assessmentMethod.update({
+        where: { id },
+        data: {
+          name: dto.name,
+          searchText: convertStringToEnglish(dto.name, true),
+          description: dto.description,
+        },
+      });
+
+      return new ResponseItem(
+        plainToInstance(ResponseAssessmentMethodDto, method, {
+          excludeExtraneousValues: true,
+        }),
+        'Cập nhật phương thức đánh giá thành công'
+      );
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        if (e.code === 'P2025') {
+          throw new NotFoundException('Không tìm thấy phương thức đánh giá');
+        }
+        if (e.code === 'P2002') {
+          throw new ConflictException('Tên phương thức đánh giá đã tồn tại.');
+        }
+      }
+      throw e;
+    }
   }
 }
